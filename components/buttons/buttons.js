98d1ad6700f1bed8b3547b537a288ba6db564b72
Chauhan Pruthviraj <ultracpp@vk.com>
2023-03-08 18:18:14 +0530

    added buttons

--- components/buttons/buttons.js
+++ components/buttons/buttons.js
@@ -3,24 +3,21 @@ import { about } from "../about/about.js";
 
 
 let aboutButton = new ultracpp("button");
-aboutButton.addAttribute("class", "nav_button");
 aboutButton.insertData("About")
-aboutButton.addEvent("click",function(){about.render()})
+aboutButton.addEvent("click", function () { about.render() })
 
 let blogButton = new ultracpp("button");
-blogButton.addAttribute("class", "nav_button");
 blogButton.insertData("Blog");
 
 let docsButton = new ultracpp("button");
-docsButton.addAttribute("class", "nav_button");
 docsButton.insertData("Docs");
 
 let ucppjsButton = new ultracpp("button");
-ucppjsButton.addAttribute("class", "nav_button");
+ucppjsButton.insertData("UCPPJS");
 
 
 let cppButton = new ultracpp("button");
-cppButton.addAttribute("class", "nav_button");
+cppButton.insertData("C++");
 
 
 
--- components/home/nav.js
+++ components/home/nav.js
@@ -1,3 +1,7 @@
-import { aboutButton } from "../buttons/buttons.js";
+import { aboutButton, blogButton, docsButton, ucppjsButton, cppButton } from "../buttons/buttons.js";
 
 aboutButton.render(".nav");
+blogButton.render(".nav");
+cppButton.render(".nav");
+docsButton.render(".nav");
+ucppjsButton.render(".nav");
