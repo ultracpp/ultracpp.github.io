be25f646ee6d67702eb41585ec2f07bc7f237ef8
Chauhan Pruthviraj <ultracpp@vk.com>
2023-03-09 18:05:15 +0530

    added some data

--- components/about/about.js
+++ components/about/about.js
@@ -1,6 +1,21 @@
 import { ultracpp } from "../../media/js/ucpp.js";
 
 const about = new ultracpp;
-about.insertData("<h1>About</h1>");
+about.addAttribute("class", "about");
 
+const style = new ultracpp("style");
+const styleData = ".about {padding:20px; min-height:100vh; max-width:100%; width:100%; margin:auto; top:auto; left:auto; display:block; bottom:auto; right:auto; }.about > p::after{content:'.'}";
+style.insertData(styleData);
+
+
+const data = "<h1>About us </h1>" +
+    "<p>C++ is Powerful Technology, better usage of computing power on machine's Resources is possible through C++ </p>" +
+    "<p> Ultracpp is a place on internet where C++ programming related content will be published by Chauhan Pruthviraj </p>" +
+    "<p>Chauhan Pruthviraj usually writes blog on C++ Programming and Release on this website. </p>" +
+    "<p>C++ is still adopting new Features as per need of modern software development paradigm </p>";
+
+
+
+about.insertData(data);
+about.insertChild(style);
 export { about }
