70cc82a35050d0d5d5fe292ce17250a6a1886832
Chauhan Pruthviraj <ultracpp@vk.com>
2023-03-09 22:14:52 +0530

    added data

---
+++ components/hindi/data/config.js
@@ -0,0 +1,64 @@
+import { data } from "./data.js"
+
+let length = data.length - 1; // length
+let slideFrom = 0; // slideFrom
+let slideTo = 2; // slideTo
+// =============
+
+let initialIndex = 0;
+
+const showFullScreen = function (initialIndex) {
+    document.querySelector(".list").innerHTML = data[initialIndex]["title"];
+}
+
+const createElement = function (data, initialIndex) {
+    let element = document.createElement("div");
+    element.setAttribute("class", "blog-screen");
+    element.innerHTML = data;
+    element.addEventListener("click", function () { showFullScreen(initialIndex) });
+    return element;
+}
+//  =============
+
+let insert = function (element) {
+    document.querySelector(".list").append(element);
+}
+
+let show = function (slideFromIndex, length) {
+    let f1 = slideFromIndex;
+    let f2 = length;
+
+    for (slideFromIndex; slideFromIndex < length; slideFromIndex++) {
+        if (data[slideFromIndex] && data[slideFrom]["title"]) {
+            insert(createElement(data[slideFromIndex]["title"], data[slideFromIndex]["initialIndex"]));
+        }
+    }
+    slideFrom = f1;
+    slideTo = f2;
+}
+
+show(slideFrom, slideTo);
+
+let next = function () {
+    console.log("====>", data[initialIndex])
+    if (initialIndex < length) {
+        document.querySelector(".list").innerHTML = null;
+        initialIndex++;
+        slideFrom++;
+        slideTo++;
+        show(slideFrom, slideTo)
+    }
+}
+
+let prev = function () {
+    if (initialIndex > 0) {
+        document.querySelector(".list").innerHTML = null;
+        initialIndex--;
+        slideFrom--;
+        slideTo--;
+        show(slideFrom, slideTo)
+    }
+}
+window.show = show;
+window.prev = prev;
+window.next = next;
---
+++ components/hindi/data/data.js
@@ -0,0 +1,53 @@
+
+/***
+ * Data for Hindi Blog
+  ***/
+
+const data = [
+    {
+        index: 0,
+        title: "C++23",
+    },
+    {
+        index: 1,
+        title: "UCPPJS",
+    },
+    {
+        index: 2,
+        title: "Gaming is Curious",
+    },
+    {
+        index: 3,
+        title: "C++ GUI",
+    },
+    {
+        index: 4,
+        title: "Ultra C++",
+    },
+    {
+        index: 5,
+        title: "Future of IT Industry",
+    },
+    {
+        index: 6,
+        title: "Why C++ is Still Relevant",
+    },
+    {
+        index: 7,
+        title: "Linux Overview",
+    },
+    {
+        index: 8,
+        title: "Ubuntu Mint",
+    },
+    {
+        index: 9,
+        title: "Csharp",
+    },
+    {
+        index: 10,
+        title: "opengl",
+    },
+];
+
+export { data }
